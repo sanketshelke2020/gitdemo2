72f4b397da6a4c516bda33b2d5697d5725ddfb20
Sanket Shelke <sanketshelke1317@gmail.com>
2022-10-12 12:59:29 +0530

    Revert "Revert "Login Funtionality""
    This reverts commit f0ad5b499ce3a9de469f15702c7de11b3c5ca64a.

---
+++ src/API/MyCleanProject1.Api/Controllers/v3/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCleanProject1.Api.Controllers.v3
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
